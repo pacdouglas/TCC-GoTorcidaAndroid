3961500e055df4b2ab4c627fc0986e16c032643c
Douglas Martins <douglas.pac@gmail.com>
2016-10-03 18:50:53 -0300

    Implementação calendario na tela de cadastro

--- app/src/main/java/br/com/gotorcida/gotorcida/activity/RegisterActivity.java
+++ app/src/main/java/br/com/gotorcida/gotorcida/activity/RegisterActivity.java
@@ -1,86 +1,92 @@
 package br.com.gotorcida.gotorcida.activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.InputType;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
 
 import br.com.gotorcida.gotorcida.R;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class RegisterActivity extends AppCompatActivity implements OnClickListener   {
+public class RegisterActivity extends AppCompatActivity  {
 
-    private DatePickerDialog datePickerDialog;
-    private SimpleDateFormat dateFormatter;
-    private EditText vtnc;
+    private DatePicker dpResult;
+    private EditText edtDateOfBirth;
+
+    private int year;
+    private int month;
+    private int day;
+
+    static final int DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        dpResult = (DatePicker) findViewById(R.id.dpResult);
+        edtDateOfBirth = (EditText) findViewById(R.id.input_dateOfBirth);
 
-        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
-        vtnc = (EditText) findViewById(R.id.meuPAU);
-        vtnc.requestFocus();
-        vtnc.setOnClickListener(new OnClickListener() {
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        edtDateOfBirth.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-                Calendar newCalendar = Calendar.getInstance();
-                datePickerDialog = new DatePickerDialog(RegisterActivity.this, new DatePickerDialog.OnDateSetListener() {
-
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        Calendar newDate = Calendar.getInstance();
-                        newDate.set(year, monthOfYear, dayOfMonth);
-                        vtnc.setText(dateFormatter.format(newDate.getTime()));
-                    }
-
-                },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+            public void onFocusChange(View v, boolean hasFocus) {
+                hideKeyboard(RegisterActivity.this, edtDateOfBirth);
+                showDialog(DATE_DIALOG_ID);
             }
         });
-    }
 
+    }
     @Override
-    public void onClick(View view) {
-        datePickerDialog.show();
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                // set date picker as current date
+                return new DatePickerDialog(this, datePickerListener,
+                        year, month,day);
+        }
+        return null;
     }
 
+    private DatePickerDialog.OnDateSetListener datePickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+
+            // set selected date into textview
+            edtDateOfBirth.setText(new StringBuilder().append(day)
+                    .append("-").append(month + 1).append("-").append(year)
+                    .append(" "));
+
+            // set selected date into datepicker also
+            dpResult.init(year, month, day, null);
+
+        }
+    };
+
+    public static void hideKeyboard(Context context, View editText) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
 }
 
