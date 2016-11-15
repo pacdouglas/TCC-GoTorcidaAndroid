0b170d2c4a232125a113c9335b4f7a21d50b353a
Douglas Martins <douglas.pac@gmail.com>
2016-11-15 21:59:32 -0200

    Ajustes no formulario de email. Funcionando agora

--- app/src/main/java/br/com/gotorcida/gotorcida/dialog/user/MyTeamHereDialog.java
+++ app/src/main/java/br/com/gotorcida/gotorcida/dialog/user/MyTeamHereDialog.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -19,6 +20,12 @@ import br.com.gotorcida.gotorcida.utils.MailSender;
 public class MyTeamHereDialog extends DialogFragment {
     Spinner mSpinner;
     View mView;
+    EditText mSportName;
+    EditText mTeamName;
+    EditText mTel;
+    Spinner mState;
+    EditText mCity;
+    EditText mObs;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -28,6 +35,14 @@ public class MyTeamHereDialog extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         mView = inflater.inflate(R.layout.dialog_my_team_here, null);
 
+        mSportName = (EditText) mView.findViewById(R.id.my_team_here_edit_text_sport);
+        mTeamName = (EditText) mView.findViewById(R.id.my_team_here_edit_text_team_name);
+        mTel = (EditText) mView.findViewById(R.id.my_team_here_edit_text_contact);
+        mState = (Spinner) mView.findViewById(R.id.my_team_here_spinner_state);
+        mCity = (EditText) mView.findViewById(R.id.my_team_here_edit_text_city);
+        mObs = (EditText) mView.findViewById(R.id.my_team_here_edit_text_obs);
+
+
         mSpinner = (Spinner) mView.findViewById(R.id.my_team_here_spinner_state);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.states_array, android.R.layout.simple_spinner_item);
@@ -36,7 +51,13 @@ public class MyTeamHereDialog extends DialogFragment {
         builder.setView(mView);
         builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                SendMailTask sendMail = new SendMailTask();
+                String auxBody = "Esporte :" + mSportName.getText().toString()
+                                + "\n\n Nome do Time: " + mTeamName.getText().toString()
+                                + "\n\n Telefone: " + mTel.getText().toString()
+                                + "\n\n Cidade: " + mCity.getText().toString() + " - "+mState.getSelectedItem().toString()
+                                + "\n\n Obs: " + mObs.getText().toString();
+
+                SendMailTask sendMail = new SendMailTask(mTeamName.getText().toString(), auxBody);
                 sendMail.execute();
             }
         });
@@ -51,28 +72,28 @@ public class MyTeamHereDialog extends DialogFragment {
 
     public class SendMailTask extends AsyncTask {
         boolean send = true;
+        String bodyMail = "";
+        String subjectMail = "";
+
+        public SendMailTask(String subjectMail, String bodyMail) {
+            this.bodyMail = bodyMail;
+            this.subjectMail = subjectMail;
+        }
+
         protected void onPreExecute() {
             Toast.makeText(mView.getContext(), "Enviando mensagem...", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         protected Object doInBackground(Object... args) {
-            try {
-                MailSender sender = new MailSender("gotorcida8cco@gmail.com", "equipegotorcida321");
-                sender.sendMail("EMAIL DO APP",
-                        "AE FOI SAPORRA",
-                        "douglas.pac@gmail.com",
-                        "ricardo.zanardo@yahoo.com");
-            } catch (Exception e) {
-                Log.e("SendMail", e.getMessage(), e);
-                send = false;
-            }
+            MailSender mailSender = new MailSender(getContext(), "douglas.pac@gmail.com", "Proposta GoTorcida - "+subjectMail, bodyMail);
+            send = mailSender.send();
             return null;
         }
 
         @Override
         public void onPostExecute(Object result) {
-            if(!send){
+            if(send){
                 Toast.makeText(mView.getContext(), "Mensagem Enviada Com Sucesso! Aguarde nosso retorno", Toast.LENGTH_LONG).show();
             }else{
                 Toast.makeText(mView.getContext(), "Erro no envio da mensagem. Tente novamente", Toast.LENGTH_LONG).show();
--- app/src/main/java/br/com/gotorcida/gotorcida/utils/MailSender.java
+++ app/src/main/java/br/com/gotorcida/gotorcida/utils/MailSender.java
@@ -1,108 +1,67 @@
 package br.com.gotorcida.gotorcida.utils;
 
-/**
- * Created by dougl on 15/10/2016.
- */
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
+import android.app.ProgressDialog;
+import android.content.Context;
+
+import java.util.Properties;
+
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.Security;
-import java.util.Properties;
+
 
 public class MailSender extends javax.mail.Authenticator{
-    private String mailhost = "smtp.gmail.com";
-    private String user;
-    private String password;
+    private Context context;
     private Session session;
 
-    static {
-        Security.addProvider(new JSSEProvider());
+    private ProgressDialog Dialog;
+
+    private String email;
+    private String subtitle;
+    private String message;
+
+    public MailSender(Context context, String email, String subtitle, String message) {
+        this.context = context;
+        this.email = email;
+        this.subtitle = subtitle;
+        this.message = message;
     }
 
-    public MailSender(String user, String password) {
-        this.user = user;
-        this.password = password;
-
+    public boolean send(){
         Properties props = new Properties();
-        props.setProperty("mail.transport.protocol", "smtp");
-        props.setProperty("mail.host", mailhost);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.socketFactory.fallback", "false");
-        props.setProperty("mail.smtp.quitwait", "false");
 
-        session = Session.getDefaultInstance(props, this);
-    }
+        session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("gotorcida8cco@gmail.com", "equipegotorcida321");
+                    }
+                });
 
-    protected PasswordAuthentication getPasswordAuthentication() {
-        return new PasswordAuthentication(user, password);
-    }
+        try {
 
-    public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception {
-        try{
-            MimeMessage message = new MimeMessage(session);
-            DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain"));
-            message.setSender(new InternetAddress(sender));
-            message.setSubject(subject);
-            message.setDataHandler(handler);
-            if (recipients.indexOf(',') > 0)
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
-            else
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
-            Transport.send(message);
-        }catch(Exception e){
+            MimeMessage mm = new MimeMessage(session);
+
+            mm.setFrom(new InternetAddress("gotorcida8cco@gmail.com"));
+            mm.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            mm.setSubject(subtitle);
+            mm.setText(message);
+
+            Transport.send(mm);
 
         }
-    }
-
-    public class ByteArrayDataSource implements DataSource {
-        private byte[] data;
-        private String type;
-
-        public ByteArrayDataSource(byte[] data, String type) {
-            super();
-            this.data = data;
-            this.type = type;
+        catch (MessagingException e)
+        {
+            return false;
         }
-
-        public ByteArrayDataSource(byte[] data) {
-            super();
-            this.data = data;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getContentType() {
-            if (type == null)
-                return "application/octet-stream";
-            else
-                return type;
-        }
-
-        public InputStream getInputStream() throws IOException {
-            return new ByteArrayInputStream(data);
-        }
-
-        public String getName() {
-            return "ByteArrayDataSource";
-        }
-
-        public OutputStream getOutputStream() throws IOException {
-            throw new IOException("Not Supported");
-        }
+        return true;
     }
 }
